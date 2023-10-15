fa86d8ae8b71c27311d5ac92e818b3bd9371431a
Bharat Kandrigi <bharatkandrigi@gmail.com>
2023-10-14 23:55:55 -0500

    Final Commit

--- FEEDBACK.md
+++ FEEDBACK.md
@@ -1,12 +1,20 @@
 # Feedback
 
-1. Your team: Team Crusader
-2. Name of each individual participating: Bharat Kandrigi (self)
+1. Your team: 
+    Team Crusader
+2. Name of each individual participating:
+    Bharat Kandrigi (self)
 3. How many unit tests were you able to pass? 
+    10
 4. Document and describe any enhancements included to help the judges properly grade your submission.
-    - Example One
-    - Example Two
-    - Example Three
+
+    Link: https://github.com/bharatkandrigi16/2023-StateFarm-CodingCompetition-python
+
+    For running the unit tests and verifying execution: I had to move 'sample_data_tool.py' and the 'data' folder with 4 csv files into 'test' due to relative import errors.
+
+    Added Flask imports and a 3rd party graphing library to visualize data using basic REST API endpoints.
+
+    Just need to run the 'application.py' file to run the Flask app
 
 5. Any feedback for the coding competition? Things you would like to see in future events?
 
--- application.py
+++ application.py
@@ -1,1 +1,38 @@
 #Create REST API
+
+from flask import Flask, jsonify, request
+import simple_data_tool
+import plotly.graph_objects as go
+
+app = Flask(__name__)
+sdt = None
+
+@app.route('/',methods=['GET','POST'])
+def home():
+    if request.method == 'GET':
+        data = "Welcome to SF Coding Competition 2023!"
+        return jsonify({'data':data})
+    
+@app.route('/home/display_data')
+def display_data():
+    #Display most spoken languages by agents in the state with the most and least disasters
+    state_with_least_disasters = sdt.get_state_with_least_disasters()
+    state_with_most_disasters = sdt.get_state_with_most_disasters()
+
+    #Data for a variety of states
+    dp1 = sdt.get_num_disasters_for_state(state_with_least_disasters)
+    dp2 = sdt.get_num_disasters_for_state(state_with_most_disasters)
+    dp3 = sdt.get_num_disasters_for_state('Alaska')
+    dp4 = sdt.get_num_disasters_for_state('Oklahoma')
+    dp5 = sdt.get_num_disasters_for_state('Texas')
+    dp6 = sdt.get_num_disasters_for_state('Florida')
+    dp7 = sdt.get_num_disasters_for_state('California')
+    dp8 = sdt.get_num_disasters_for_state('New York')
+    fig = go.Figure(data=[go.Bar(x=[state_with_least_disasters,'Alaska','Oklahoma','Texas','Florida','California','New York'], y=[dp1,dp2,dp3,dp4,dp5,dp6,dp7,dp8])])
+    fig.show()
+    return jsonify({'data': fig})
+
+if __name__ == "__main__":
+    #Initialize unit test class to get data
+    sdt = simple_data_tool.SimpleDataTool()
+    app.run(debug=True)
